867fe86c9444d83a5fd0755737d4a9192f715062
AllanMoorhouse23 <allanmoorhouse.am@gmail.com>
2019-02-07 17:02:05 +1300

    Add project files.

---
+++ DirectorySearcher/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace DirectorySearcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.textBoxSearchPath = new System.Windows.Forms.TextBox();
+            this.backgroundSearcher = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFileCount = new System.Windows.Forms.Label();
+            this.lblPercentage = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(940, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(940, 323);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(12, 40);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxOutput.Size = new System.Drawing.Size(1003, 276);
+            this.textBoxOutput.TabIndex = 3;
+            // 
+            // textBoxSearchPath
+            // 
+            this.textBoxSearchPath.Location = new System.Drawing.Point(64, 14);
+            this.textBoxSearchPath.Name = "textBoxSearchPath";
+            this.textBoxSearchPath.Size = new System.Drawing.Size(750, 20);
+            this.textBoxSearchPath.TabIndex = 4;
+            // 
+            // backgroundSearcher
+            // 
+            this.backgroundSearcher.WorkerReportsProgress = true;
+            this.backgroundSearcher.WorkerSupportsCancellation = true;
+            this.backgroundSearcher.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundSearcher_DoWork);
+            this.backgroundSearcher.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundSearcher_ProgressChanged);
+            this.backgroundSearcher.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundSearcher_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 323);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(771, 23);
+            this.progressBar1.TabIndex = 5;
+            this.progressBar1.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(827, 328);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "File Count:";
+            // 
+            // lblFileCount
+            // 
+            this.lblFileCount.AutoSize = true;
+            this.lblFileCount.Location = new System.Drawing.Point(890, 328);
+            this.lblFileCount.Name = "lblFileCount";
+            this.lblFileCount.Size = new System.Drawing.Size(37, 13);
+            this.lblFileCount.TabIndex = 7;
+            this.lblFileCount.Text = "00000";
+            // 
+            // lblPercentage
+            // 
+            this.lblPercentage.AutoSize = true;
+            this.lblPercentage.BackColor = System.Drawing.Color.Transparent;
+            this.lblPercentage.Location = new System.Drawing.Point(788, 328);
+            this.lblPercentage.Name = "lblPercentage";
+            this.lblPercentage.Size = new System.Drawing.Size(33, 13);
+            this.lblPercentage.TabIndex = 8;
+            this.lblPercentage.Text = "000%";
+            this.lblPercentage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(820, 16);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Include Subfolders";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Directory:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 359);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lblPercentage);
+            this.Controls.Add(this.lblFileCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBoxSearchPath);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Directory Searcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.TextBox textBoxSearchPath;
+        private System.ComponentModel.BackgroundWorker backgroundSearcher;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFileCount;
+        private System.Windows.Forms.Label lblPercentage;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DirectorySearcher/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DirectorySearcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> _files = new List<string>();
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string directory;
+            directory = textBoxSearchPath.Text;
+            currentIndex = 0;
+           
+            backgroundSearcher.RunWorkerAsync(argument: directory);
+        }
+
+        int percentage;
+        int currentIndex;
+        int fileCount;
+        string output;
+
+        void DirSearch(string sDir)
+        {
+            try
+            {
+                
+                backgroundSearcher.ReportProgress(percentage);
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    foreach (string f in Directory.GetFiles(d))
+                    {
+                        string test = f.Replace("\\" , @"/");
+                       // Console.WriteLine(f);
+                        //backgroundSearcher.ReportProgress();
+                        output += test + "\r\n";
+                       
+                        currentIndex++;
+                        percentage = (currentIndex + 1) * 100 / fileCount;
+                    }
+                    //Searches the sub folders by calling the search again
+                    DirSearch(d);
+                }
+
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            //Create Stream writer
+            StreamWriter writer;
+            //Catch any errors that may occur
+            try
+            {
+                //Create filter for save dialog
+                const string FILTER = "Text Files|*.txt|All Files|*.*";
+                //Set filter for save dialog
+                saveFileDialog1.Filter = FILTER;
+                //Open save file dialog and if the ok button was pressed
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    //Sets the write location to the save dialog file name
+                    writer = File.CreateText(saveFileDialog1.FileName);
+                    //Write all of the formated text from the textbox to the file
+                    writer.Write(textBoxOutput.Text);
+                    //Close the file
+                    writer.Close();
+                }
+            }
+            //If any errors occured
+            catch (Exception ex)
+            {
+                //Display error message
+                MessageBox.Show(ex.Message);
+            }
+        }
+        
+        private void backgroundSearcher_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string directory = (string)e.Argument;
+            fileCount = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories).Length;
+            //Console.WriteLine("File count: " + fileCount);
+            output = "";
+            DirSearch(directory);
+        }
+
+        private void backgroundSearcher_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //Console.WriteLine("Done");
+            lblFileCount.Text = fileCount.ToString();
+            progressBar1.Value = 100;
+            lblPercentage.Text = "100%";
+            textBoxOutput.Text = output;
+        }
+
+        private void backgroundSearcher_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            lblPercentage.Text = e.ProgressPercentage.ToString() + "%";
+            //Console.WriteLine(e.ProgressPercentage);
+               
+        }
+    }
+}
